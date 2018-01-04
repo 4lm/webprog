d45bc2b51eac189edd833d48d6931206685c843e
Alexis Michaltsis <alexis@michaltsis.net>
2018-01-04 18:47:23 +0100

    Clean up and add some comments

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,17 +1,16 @@
-var words = [];
-var score = [];
-var max = 0;
-var divide = 1;
-var counter;
-var fill = d3.scale.category20();
+var fill = d3.scale.category20(); // color scheme 
 
-function fetchJSON(words_value) {
+function fetchJSONandDraw(words_value) {
+  words_value = words_value.replace(/\s/g, "+"); // for API call
+  // Use of proxy server to add CORS to original API response
   var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   var targetUrl = 'https://api.datamuse.com/words?ml=' + words_value;
-  words = [];
-  score = [];
-  max = 0;
-  divide = 1;
+  var words = []; // Array of words list
+  var score = []; // Array of score of word list
+  var max = 0; // Variable for holding the max score value
+  var divide = 1; // Divide factor, initiated with the value 1
+  var counter = -1;
+  // Fetch from API
   fetch(proxyUrl + targetUrl)
     .then(blob => blob.json())
     .then(data => {
@@ -26,19 +25,21 @@ function fetchJSON(words_value) {
       console.log(max);
       words.unshift(words_value);
       score.unshift(100000);
-      counter = 0;
+      // Define layout
       var layout = d3.layout.cloud()
         .size([1200, 900])
-        .words(words.map(function (d) {
+        .words(words.map(function (word) {
             counter++;
-            return { text: d, size: score[counter - 1] / divide };
+            return { text: word, size: score[counter] / divide };
           }))
         .padding(5)
-        .rotate(function () { return ~~(Math.random() * 2) * 90; })
+        .rotate(() => ~~(Math.random() * 2) * 90)
         .font("Impact")
-        .fontSize(function (d) { return d.size; })
+        .fontSize(d => d.size)
         .on("end", draw);
+
       layout.start();
+      // Draw
       function draw(words) {
         document.getElementById("cloud").innerHTML = "";
         d3.select("#cloud").append("svg")
@@ -49,14 +50,12 @@ function fetchJSON(words_value) {
           .selectAll("text")
           .data(words)
           .enter().append("text")
-          .style("font-size", function (d) { return d.size + "px"; })
+          .style("font-size", d => d.size + "px")
           .style("font-family", "Impact")
-          .style("fill", function (d, i) { return fill(i); })
+          .style("fill", (d, i) => fill(i))
           .attr("text-anchor", "middle")
-          .attr("transform", function (d) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-          })
-          .text(function (d) { return d.text; });
+          .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
+          .text(d => d.text);
       }
       return data;
     })
@@ -78,7 +77,7 @@ document.getElementById("button").addEventListener("click", function () {
     document.getElementById("cloud").innerHTML = "<img src=\"images/ajax-loader.gif\">";
     var words_value = document.getElementById("words").value;
     words = [];
-    fetchJSON(words_value);
+    fetchJSONandDraw(words_value);
     document.getElementById("words").value = "";
   }
 });
