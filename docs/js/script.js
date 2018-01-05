c759ab686d1b6d93744d832b58d08add90406d03
Alexis Michaltsis <alexis@michaltsis.net>
2018-01-05 14:07:07 +0100

    Extract toDos

--- docs/js/script.js
+++ docs/js/script.js
@@ -183,13 +183,3 @@ document.getElementById("download-svg").addEventListener("click", function () {
     downloadSVG();
   }
 });
-
-
-/* ToDo: Licenses to credit:
-
-Download function, MIT: http://bl.ocks.org/curran/7cf9967028259ea032e8
-Fetch API code, Apache 2.0: https://github.com/google-developer-training/pwa-training-labs
-d3, BSD-3-Clause: https://github.com/d3/d3
-d3-cloud, BSD-3-Clause: https://github.com/jasondavies/d3-cloud
-
-*/
